d9b0b99bf258df5b2839de762de9862e43002626
akshith99 <ak38293@gmail.com>
2023-07-25 20:58:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import { useDispatch, useSelector } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+// import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+import { fetchCartData, sendCartData } from './store/cart-actions';
+
+let isInitial = true;
+
+function App() {
+
+
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart))
+    }
+  }, [cart, dispatch]);
+
+  //commenting this useEffect to make the app component leaner
+  //all the side effects below are now moved to cart-slice with redux action creator thunk
+
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(uiActions.showNotification({
+  //       status: 'pending',
+  //       title: 'Sending...',
+  //       message: 'Sending cart data!'
+  //     }));
+
+  //     const response = await fetch('firebase api', {
+  //       method: 'PUT',
+  //       body: JSON.stringify(cart),
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error('Sending cart data failed.');
+  //     }
+
+  //     dispatch(uiActions.showNotification({
+  //       status: 'success',
+  //       title: 'Success!',
+  //       message: 'Sent cart data successfully!'
+  //     }));
+  //   };
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) => {
+  //     dispatch(uiActions.showNotification({
+  //       status: 'error',
+  //       title: 'Error!',
+  //       message: 'Sent cart data failed!'
+  //     }));
+  //   });
+  // }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Shop/ProductItem.js
@@ -0,0 +1,79 @@
+import { useDispatch } from 'react-redux';
+import Card from '../UI/Card';
+import classes from './ProductItem.module.css';
+import { cartActions } from '../../store/cart-slice';
+
+const ProductItem = (props) => {
+
+  // const cart = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  /*this approach for sending http request is not recommended 
+   because we are doing a lot of work in the component and making less use of redux */
+
+  // const addToCartHandler = () => {
+
+  //   const newTotalQuantity = cart.totalQuantity + 1;
+
+  //   const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+
+  //   const existingItem = updatedItems.find((item) => item.id === id);
+
+  //   if (existingItem) {
+
+  //     const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+  //     updatedItem.quantity++;
+  //     updatedItem.totalPrice = updatedItem.totalPrice + price;
+  //     const existingItemIndex = updatedItems.findIndex((item) => item.id === id);
+  //     updatedItems[existingItemIndex] = updatedItem;
+  //   } else {
+  //     updatedItems.push({
+  //       id: id,
+  //       price: price,
+  //       quantity: 1,
+  //       totalPrice: price,
+  //       name: title
+  //     });
+  //   }
+
+  //   const newCart = {
+  //     totalQuantity: newTotalQuantity,
+  //     items: updatedItems
+  //   };
+
+  //   dispatch(cartActions.replaceCart(newCart));
+
+  //   // and then send Http request
+  //   fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+  // };
+
+  const addToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id: id,
+      title: title,
+      price: price,
+      description: description
+    }));
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCartHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
